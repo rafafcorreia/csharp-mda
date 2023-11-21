be3af730daca2077820aabb76188a06cfc57bad1
rafafcorreia <rafa.f.correia@gmail.com>
2023-11-21 20:28:17 -0300

    Primeiro commit

---
+++ SelectProductMDA.cs
@@ -0,0 +1,66 @@
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.MultiTouch;
+
+namespace appium;
+
+
+public class SelectProductMDA
+{
+
+    public static string SAUCE_USERNAME = Environment.GetEnvironmentVariable("SAUCE_USERNAME");
+
+    public static string SAUCE_ACCESS_KEY = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY");
+
+    public Uri URI = new Uri($"https://{SAUCE_USERNAME}:{SAUCE_ACCESS_KEY}@ondemand.us-west-1.saucelabs.com:443/wd/hub");
+    public AndroidDriver<AndroidElement> driver { get; set; }
+
+    [SetUp]
+    public void MobileBaseSetup()
+    {
+        var options = new AppiumOptions();
+        options.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+        options.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "9.0");
+        options.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Samsung Galaxy S9 FHD GoogleAPI Emulator");
+        options.AddAdditionalCapability(MobileCapabilityType.App, "storage:filename=mda-1.0.17-20.apk");
+        options.AddAdditionalCapability("newCommandTimeout", 90);
+
+        driver = new AndroidDriver<AndroidElement>(remoteAddress: URI, driverOptions: options, commandTimeout: TimeSpan.FromSeconds(180));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (driver == null) return;
+        driver.Quit();
+    }
+
+    [TestCase]
+    public void SelecionarProdutoMDA()
+    {
+        Assert.That(driver.FindElement(MobileBy.AccessibilityId("App logo and name")).Displayed, Is.True);
+
+        driver.FindElement(MobileBy.AccessibilityId("Sauce Lab Back Packs")).Click();
+
+        String tituloProduto = driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/productTV")).Text;
+        Assert.That(tituloProduto, Is.EqualTo("Sauce Lab Back Packs"));
+
+        String precoProduto = driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/priceTV")).Text;
+        Assert.That(precoProduto, Is.EqualTo("$ 29.99"));
+
+        TouchAction actionOne = new TouchAction(driver);
+        actionOne.Press(500, 1400);
+        actionOne.MoveTo(500, 700);
+        actionOne.Release();
+        actionOne.Perform();
+
+        driver.FindElement(MobileBy.AccessibilityId("Tap to add product to cart")).Click();
+
+        String numeroCarrinho = driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/cartTV")).Text;
+        Assert.That(numeroCarrinho, Is.EqualTo("1"));
+
+    }
+
+
+}
